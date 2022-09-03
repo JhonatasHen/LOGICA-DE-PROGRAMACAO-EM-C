22dbd27abb01e6bc563070b90431c22d1b0d185a
JhonatasHen <jhonatas.hen97@gmail.com>
2022-09-03 08:40:40 -0300

    Atualizaçã ocódigo

--- LOGICA EM C/EX002 ANO MAIS RECENTE.c
+++ LOGICA EM C/EX002 ANO MAIS RECENTE.c
@@ -7,25 +7,25 @@ int main (){
     int ano,dia,mes;
     int ano2,dia2,mes2;
 
-    printf("Primeira data dia/mês/ano : ");
+    printf("Primeira data dia/mï¿½s/ano : ");
     scanf("%d""%d""%d", &dia, &mes, &ano);
     system("cls");
 
-    printf("Segunda data dia/mês/ano : ");
+    printf("Segunda data dia/mï¿½s/ano : ");
     scanf("%d""%d""%d", &dia2, &mes2, &ano2);
     system("cls");
 
     if (ano > ano2){
-        printf("A data %d/%d/%d é maior\n", dia, mes, ano);
+        printf("A data mais recente Ã© %d/%d/%d.\n", dia, mes, ano);
     }
     else if(ano == ano2 && mes > mes2){
-        printf("A data %d/%d/%d é maior\n", dia, mes, ano);
+        printf("A data mais recente Ã© %d/%d/%d.\n", dia, mes, ano);
     }
     else if(ano == ano2 && mes == mes2 && dia > dia2){
-        printf("A data %d/%d/%d é maior\n", dia, mes, ano);
+        printf("A data mais recente Ã© %d/%d/%d.\n", dia, mes, ano);
     }
     else{
-        printf("A data %d/%d/%d é maior\n", dia2, mes2, ano2);
+        printf("A data mais recente Ã© %d/%d/%d.\n", dia2, mes2, ano2);
     }
     return 0;
 }
