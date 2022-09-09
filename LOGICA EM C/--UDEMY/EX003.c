4d27073e72e43254191f6de5f7ed6d56ce45c9fc
JhonatasHen <jhonatas.hen97@gmail.com>
2022-09-09 13:33:43 -0300

    cod

---
+++ LOGICA EM C/--UDEMY/EX003.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main (){
+    setlocale(LC_ALL, "Portuguese");
+    int con;
+    con = 1;
+    float n1, raiz, potencia;
+    do{
+        printf("Didite um número: ");
+        scanf("%f", &n1);
+
+        if (n1 > 0)
+        {
+            raiz = sqrt(n1);
+            potencia = pow(n1, 2);
+            printf("A raiz quadrada de %.0f é %.2f eo quadrado dele é %.0f\n", n1, raiz, potencia);
+        }
+        else
+        {
+            printf("Digite um número valido....\n");
+        }
+    }while (con < 1);
+    
+
+    return 0;
+}
