c9b44842539b09c0b267f167f3581ae93dd98c2a
JhonatasHen <jhonatas.hen97@gmail.com>
2022-09-12 13:30:26 -0300

    att

--- LOGICA EM C/--FACUL/EX012  Faça um algoritmo que leia o nome, o salário e a quantidade de horas trabalhadas  por dia de 5 funcionários..c
+++ LOGICA EM C/--FACUL/EX012  Faça um algoritmo que leia o nome, o salário e a quantidade de horas trabalhadas  por dia de 5 funcionários..c
@@ -4,11 +4,11 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int i, horas;
-    float salario;
-    char nome [50];
+    int horas[3];
+    float salario[3];
+    char nome [3][50];
 
-    for (size_t i = 1; i <= 5; i++)
+    for (int i = 1; i < 3; i++)
     {
         fflush(stdin);
         printf("Nome do funcionário: ");
@@ -19,26 +19,10 @@ int main()
         scanf("%d", &horas);
         printf("\n");
         //system("cls");
-
-        if (horas <= 2)
-        {
-            printf("%s, tarabalhou %dhrs\nRecebe R$%.2f\n", nome, horas, salario);
-            printf("\n");
-        }
-        else if (horas > 2 && horas <= 4)
-        {
-            salario = salario * 0.20 + salario;
-            printf("%s, tarabalhou %dhrs tera 20%% acréscimo\nRecebe R$%.2f\n", nome, horas, salario);
-            printf("\n");
-        }
-        else
-        {
-            salario = salario * 0.30 + salario;
-            printf("%s, tarabalhou %dhrs tera 30%% de acréscimo\nRecebe R$%.2f\n", nome, horas, salario);
-            printf("\n");
-        }
-        
-        
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("nome", nome[i]);
     }
     
     return 0;
