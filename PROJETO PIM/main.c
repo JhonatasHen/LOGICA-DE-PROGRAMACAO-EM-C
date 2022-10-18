8fef22abee2f44a175291569939aea0c3edbea58
JhonatasHen <jhonatas.hen97@gmail.com>
2022-10-17 21:14:57 -0300

    ATUALIZAÇÃO

--- PROJETO PIM/main.c
+++ PROJETO PIM/main.c
@@ -12,11 +12,17 @@ struct ts_funcionario
     char jornada_de_trabalho[25];
 }funcionario;
 
+struct ts_cliente
+{
+
+}cliente;
 
 int main()
 {
     setlocale(LC_ALL, "");
     FILE *cadastro_funcionario;
+    FILE *cadastro_cliente;
+
     char *resultado_nome;
 
     //- - -LOGIN- - -
@@ -63,105 +69,107 @@ int main()
 
 
     //- - -TERMINAL ADMINISTRADOR- - -
-
+    int  menu = 0;
     if(terminal_admin == 1)
     {
         int opcao;
         system("cls");
-        printf("ACESSO ADMINSTRADOR\n\n");
+        do{
+            system("cls");
+            printf("ACESSO ADMINSTRADOR\n\n");
+            printf("[1] Cadastros de funcionários\n[2] Cadastros de clientes\n[3] Pagamento\n[4] funcionários cadastrados\n");
+            printf("Escolha uma opção:\n");
+            scanf("%d", &opcao);
+            fflush(stdin);
 
-        printf("[1] Cadastros de funcionários\n[2] Cadastros de clientes\n[3] Pagamento\n[4] funcionários cadastrados");
-        printf("Escolha uma opção:\n");
-        scanf("%d", &opcao);
-        fflush(stdin);
+            switch(opcao)
+            {
+                //CADASTRO DE FUNCIONÁRIO
+                case 1:
+                    system("cls");
+                    cadastro_funcionario = fopen("Funcionarios.txt", "a");
 
-        switch(opcao)
-        {
-            //CADASTRO DE FUNCIONÁRIO
-            case 1:
-
-                cadastro_funcionario = fopen("Funcionarios.txt", "a");
-
-                if(cadastro_funcionario)
-                {
-                    int novo_registro = 0;
-                    while(novo_registro == 0)
+                    if(cadastro_funcionario)
                     {
-                        system("cls");
-                        printf("Nome do funcionário\n");
-                        fgets(funcionario.nome, 100, stdin);
-
-
-                        printf("Data de nascimento:\n");
-                        printf("Dia: ");
-                        scanf("%d", &funcionario.dia);
-                        fflush(stdin);
-                        printf("Mês: ");
-                        scanf("%d", &funcionario.mes);
-                        fflush(stdin);
-                        printf("ano: ");
-                        scanf("%d", &funcionario.ano);
-                        fflush(stdin);
-
-                        printf("Cargo: ");
-                        fgets(funcionario.cargo, 25, stdin);
-
-                        printf("Jornada de trabalho: ");
-                        fgets(funcionario.jornada_de_trabalho, 25, stdin);
-
-                        fprintf(cadastro_funcionario,"nome do funcionario: %s", funcionario.nome);
-                        fprintf(cadastro_funcionario,"Data de nascimento: %d/%d/%d\n", funcionario.dia, funcionario.mes, funcionario.ano);
-                        fprintf(cadastro_funcionario,"Cargo: %s", funcionario.cargo);
-                        fprintf(cadastro_funcionario, "Jornada de trabalho: %s\n\n", funcionario.jornada_de_trabalho);
-
-                        printf("Registrar novo funcionário\n[0] SIM\n[1] NÃO\n");
-                        scanf("%d", &novo_registro);
-                        getchar();
-                        if(novo_registro == 1)
+                        int novo_registro = 0;
+                        while(novo_registro == 0)
                         {
-                            fclose(cadastro_funcionario);
-                            novo_registro ++;
+                            system("cls");
+                            printf("Nome do funcionário\n");
+                            fgets(funcionario.nome, 100, stdin);
+
+
+                            printf("Data de nascimento:\n");
+                            printf("Dia: ");
+                            scanf("%d", &funcionario.dia);
+                            fflush(stdin);
+                            printf("Mês: ");
+                            scanf("%d", &funcionario.mes);
+                            fflush(stdin);
+                            printf("ano: ");
+                            scanf("%d", &funcionario.ano);
+                            fflush(stdin);
+
+                            printf("Cargo: ");
+                            fgets(funcionario.cargo, 25, stdin);
+
+                            printf("Jornada de trabalho: ");
+                            fgets(funcionario.jornada_de_trabalho, 25, stdin);
+
+                            fprintf(cadastro_funcionario,"nome do funcionario: %s", funcionario.nome);
+                            fprintf(cadastro_funcionario,"Data de nascimento: %d/%d/%d\n", funcionario.dia, funcionario.mes, funcionario.ano);
+                            fprintf(cadastro_funcionario,"Cargo: %s", funcionario.cargo);
+                            fprintf(cadastro_funcionario, "Jornada de trabalho: %s\n\n", funcionario.jornada_de_trabalho);
+
+                            printf("Registrar novo funcionário\n[0] SIM\n[1] NÃO\n");
+                            scanf("%d", &novo_registro);
+                            getchar();
+                            if(novo_registro == 1)
+                            {
+                                fclose(cadastro_funcionario);
+                                novo_registro ++;
+                            }
                         }
+
                     }
 
-                }
+                    break;
 
-                break;
+                //CADASTRO DE CLIENTE
+                case 2:
 
-            //CADASTRO DE CLIENTE
-            case 2:
+                    break;
 
-                break;
+                //PAGAMENTO
+                case 3:
 
-            //PAGAMENTO
-            case 3:
+                    break;
 
-                break;
-
-            //FUNCIONÁRIOS CADASTRADOS
-            case 4:
-
-                cadastro_funcionario = fopen("Funcionarios.txt", "a");
-                if(cadastro_funcionario)
-                {
-                    while(!feof(cadastro_funcionario))
+                //FUNCIONÁRIOS CADASTRADOS
+                case 4:
+                    system("cls");
+                    cadastro_funcionario = fopen("Funcionarios.txt", "r");
+                    if(cadastro_funcionario)
                     {
-                        resultado_nome = fgets(funcionario.nome, 100, cadastro_funcionario);
-                        if (resultado_nome)
+                        while(!feof(cadastro_funcionario))
                         {
-                            printf("%s\n", resultado_nome);
+                            resultado_nome = fgets(funcionario.nome, 100, cadastro_funcionario);
+                            if (resultado_nome)
+                            {
+                                printf("%s", resultado_nome);
+                            }
                         }
                     }
-                }
+                    system("Pause");
+                    fclose(cadastro_funcionario);
+                    break;
 
-                break;
-
-            default:
-                printf("Opção invalida...\n");
-                system("pause");
-                system("cls");
-        }
-
+                default:
+                    printf("Opção invalida...\n");
+                    system("pause");
+                    system("cls");
+            }
+        }while(menu == 0);
 
     }
     //- - -TERMINAL ADMINISTRADOR- - -
